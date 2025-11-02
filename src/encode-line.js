bee0c9c94645811e63971a311993e4c51319ee1b
Eduard <edyard2002@mail.ru>
2025-11-02 22:56:24 +0300

    feat: add the solution of task encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given a string, return its encoding version.
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function encodeLine(/* str */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  let res = "";
+  let count = 1;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === str[i + 1]) {
+      count += 1;
+    } else {
+      res += (count > 1 ? count : "") + str[i];
+      count = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
